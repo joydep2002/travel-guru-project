4c4bed2c158ac2fbcaa51b25e5f1dbb452e5629a
joydep2002 <joydepdhar2002@gmail.com>
2020-09-27 20:09:24 +0600

    first commit

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfPY7XgmOGLBJ9EFwjXHV6J2iV5LE9RzE",
+  authDomain: "travel-guru-56bbd.firebaseapp.com",
+  databaseURL: "https://travel-guru-56bbd.firebaseio.com",
+  projectId: "travel-guru-56bbd",
+  storageBucket: "travel-guru-56bbd.appspot.com",
+  messagingSenderId: "589889550571",
+  appId: "1:589889550571:web:5622c3b8aaeb2f7076a6ae"
+};
+export default firebaseConfig;
